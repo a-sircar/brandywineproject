df20718422eb4c02baccfb864b0dd312e2e04a28
Cloud9c <kire5030@gmail.com>
2021-01-16 23:09:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let currentTarget;
 function changeContent(target) {
 	currentTarget = target;
 	document.getElementById('popup-title').textContent = target.children[1].children[0].textContent;
-	document.getElementById('popup-author').textContent = 'Photo taken by ' + target.children[1].children[1].textContent;
 	document.getElementById('popup-img').src = target.children[0].src;
 	document.getElementById('popup-content').textContent = target.getAttribute('data-content');
 }
